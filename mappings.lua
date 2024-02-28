bbb4421e162c6f2fe885d743bd15f3fe0ad0f19e
Jake <jacob.silliker@gmail.com>
2024-02-28 10:29:44 -0400

    updates

--- mappings.lua
+++ mappings.lua
@@ -7,18 +7,23 @@ return {
   -- first key is the mode
   n = {
     -- second key is the lefthand side of the map
-
     -- navigate buffer tabs with `H` and `L`
-    -- L = {
-    --   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-    --   desc = "Next buffer",
-    -- },
-    -- H = {
-    --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-    --   desc = "Previous buffer",
-    -- },
+    L = {
+      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+      desc = "Next buffer",
+    },
+    H = {
+      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+      desc = "Previous buffer",
+    },
+    J = { "mzJ`z" },
+    n = { "nzzzv" },
+    N = { "Nzzzv" },
+    Q = { "<nop>" },
+    ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-d>"] = { "<C-d>zz" },
+    ["<C-u>"] = { "<C-u>zz" },
 
-    -- mappings seen under group name "Buffer"
     ["<leader>bD"] = {
       function()
         require("astronvim.utils.status").heirline.buffer_picker(
@@ -27,14 +32,28 @@ return {
       end,
       desc = "Pick to close",
     },
-    -- tables with the `name` key will be registered with which-key if it's installed
-    -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<leader>r"] = {
+      name = "Run test (Java)",
+    },
+    ["<leader>rt"] = {
+      function() require("jdtls").test_nearest_method() end,
+      desc = "Nearest method",
+    },
+    ["<leader>rT"] = {
+      function() require("jdtls").test_class() end,
+      desc = "Class",
+    },
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
   },
+  v = {
+    J = { ":m '>+1<CR>gv=gv", desc = "Move selection down" },
+    K = { ":m '<-2<CR>gv=gv", desc = "Move selection up" },
+  },
+  x = {
+    ["<leader>p"] = { "\"_dP", desc = "Delete selection into void, paste over" }
+  }
 }
